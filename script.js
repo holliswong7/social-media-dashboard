5b8a350cae269671dd7d81cfa72b2669434c4e3e
Hollis Wong <hiuyiwong@Hiuyis-MacBook.local>
2020-10-30 17:20:21 +0800

    Added comment and cleanup

--- script.js
+++ script.js
@@ -1,38 +1,27 @@
 function myFunc() {
-    // body.classList.toggle(".body__light");
-    // var aa = document.getElementById("a");
-    // console.log(aa.classList)
-    // var card = document.querySelector(".card");
-    // console.log(card.classList)
-
+    
+    // Change the body to light mode
     var body = document.querySelector("body");
-    // console.log(body.classList);
     body.classList.toggle("body__light");
     
+    // Change the 4 cards to light mode
     var card = document.querySelectorAll(".card");
     for (var i=0; i<card.length; i++){
         card[i].classList.toggle("card__box__light");
     }
 
+    // Change the 8 boxes to light mode
     var box = document.querySelectorAll(".box");
     for (var i=0; i<box.length; i++){
         box[i].classList.toggle("card__box__light");
     }
 
-    // var circle = document.querySelector(".button__container");
-    // console.log(circle.classList);
-    // circle.classList.toggle("button__circle__light");
-
-    // var circle = document.getElementById("circle");
+    // Change the circle position to the right
     var circle = document.querySelector(".button__circle");
-    // console.log(circle.classList);
     circle.classList.toggle("button__circle__light");
     if (circle.style.left == "22px"){
         circle.style.left = "3px";
     }else{
         circle.style.left = "22px";
     }
-    
-
-
 }
